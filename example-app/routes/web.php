103600d443fdcc9c31c6985a8d9906d3e5287bc1
SiraphopSangkrit <78173629+SiraphopSangkrit@users.noreply.github.com>
2021-09-25 22:06:47 +0700

    Update web.php

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('dashboard');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
