ca51b8710c3348647e10694b546fc5505fd2993f
SiraphopSangkrit <Siraphop_sangkrit@kkumail.com>
2021-09-25 23:23:03 +0700

    main

--- example-app/resources/views/Layouts/header.blade.php
+++ example-app/resources/views/Layouts/header.blade.php
@@ -9,13 +9,10 @@
   <title>
     CodeLab
   </title>
- 
+
   <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-
   <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-
   <link href="../assets/css/black-dashboard.css?v=1.0.0" rel="stylesheet" />
-
   <link href="/fontawesome-free-5.15.4-web/css/all.css" rel="stylesheet">
 </head>
